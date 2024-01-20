dd02580b4e02cd30be3d1721a0c53c67c96e306d
shailendrathakur300 <shailendrathakuracademic123@gmail.com>
2024-01-20 12:00:00 +0530

    Backdated commit

---
+++ app/javascript.js
@@ -0,0 +1,21 @@
+function getLength(str) {
+  console.log('Original String:', str)
+  console.log('Length:', str.length)
+}
+getLength('Hello World')
+/* To find a length
+ */
+const str = 'harkirat singh'
+console.log(str.length)
+/* To find a index */
+function findIndex(str, target) {
+  console.log('Original String:', str)
+  console.log('Index:', str.indexOf(target))
+}
+findIndex('Hello World', 'World')
+findIndex('Hello World World', 'World')
+/* noted--find /notfind */
+
+/* we learn till now - length,lastIndexof,indexof */
+//
+findIndex('Hello World World', 'World')
