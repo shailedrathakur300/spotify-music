a984f3e6f39a5c16e9800fd040292fdf31df0196
shailendrathakur300 <shailendrathakuracademic123@gmail.com>
2024-10-02 23:54:13 +0530

    done

--- app/src/app/components/Appbar.tsx
+++ app/src/app/components/Appbar.tsx
@@ -1,26 +1,33 @@
-"use client";
+'use client'
 
-import { signIn, signOut, useSession } from "next-auth/react";
+import { signIn, signOut, useSession } from 'next-auth/react'
 
 export function Appbar() {
-  const session = useSession();
+  const session = useSession()
   return (
     <div>
       <div className="flex justify-between">
         <div>Muzi</div>
         <div>
           {session.data?.user && (
-            <button className="m-2 p-2 bg-blue-400" onClick={() => signOut()}>
+            <button
+              className="m-2 p-2 bg-blue-400"
+              onClick={() => signOut()}
+            >
               Logout
             </button>
           )}
           {!session.data?.user && (
-            <button className="m-2 p-2 bg-blue-400" onClick={() => signIn()}>
+            <button
+              className="m-2 p-2 bg-blue-400"
+              onClick={() => signIn()}
+            >
               Signin
             </button>
           )}
         </div>
+        <div></div>
       </div>
     </div>
-  );
+  )
 }
